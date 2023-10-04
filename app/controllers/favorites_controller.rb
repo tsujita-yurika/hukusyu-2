068db533d0e3543efa164a9f90332153e76c0beb
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2023-10-04 10:08:25 +0000

    [Start]課題６

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,21 +1,21 @@
 class FavoritesController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    favorite= current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite= current_user.favorites.new(book_id: @book.id)
     #favorites = その時ログインしているユーザーの空のリストを作成
     favorite.save
     #いいねを保存
-    redirect_back(fallback_location: root_path)
+    #redirect_back(fallback_location: root_path)非同期化のため削除
     #直前のページにリダイレクト
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     #テーブルから呼ばれたbook.idを見つけてきてdestroyする
     favorite.destroy
-    redirect_back(fallback_location: root_path)
+    #redirect_back(fallback_location: root_path)
   end
 
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,9 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+//= require jquery
+//= require rails-ujs
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -11,7 +14,7 @@ import "jquery"
 import "popper.js"
 import "bootstrap"
 
-import "../stylesheets/application" 
+import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
