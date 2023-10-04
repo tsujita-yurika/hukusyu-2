9d1b36ec09f8db7611b8f7a82573c5f296b1248f
tsujita-yurika <143866084+tsujita-yurika@users.noreply.github.com>
2023-10-04 13:37:38 +0000

    [Update]課題６

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,21 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @user = @book.user
+    #ここ
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
+    #redirect_to request.referer
   end
 
   def destroy
-    comment = BookComment.find(params[:id])
-    comment.destroy
-    redirect_to request.referer
+    @comment = BookComment.find(params[:id])
+    @comment.destroy
+    @book = Book.find(params[:book_id])
+    # ここ
+    #redirect_to request.referer
   end
 
   private
